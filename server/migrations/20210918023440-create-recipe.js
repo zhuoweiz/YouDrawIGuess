4ca8182989ea237ef489ce89be035464e606c253
zhuowei zhang <zhuoweiz10@gmail.com>
2021-09-17 22:49:47 -0400

    [zhuoweiz] basic backend seetup

---
+++ server/migrations/20210918023440-create-recipe.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      ingredients: {
+        type: Sequelize.STRING
+      },
+      direction: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Recipes');
+  }
+};
---
+++ server/models/recipe.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Recipe.init({
+    title: {
+      type: DataTypes.STRING
+    },
+    ingredients: DataTypes.STRING,
+    direction: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Recipe',
+  });
+
+  Recipe.associate = function(models) {
+    // associations can be defined here
+    Recipe.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+  
+  return Recipe;
+};
