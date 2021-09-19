4144778112794942ba8d56dc83108c3dc3d580d7
zhuowei zhang <zhuoweiz10@gmail.com>
2021-09-19 15:06:36 -0400

    [zhuoweiz] basic socket.io setup

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,35 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const models = require('../models');
 
+
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const ioServer = http.createServer(app);
+const io = new Server(ioServer);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    const msg = "some one left";
+    io.emit('left', msg);
+    console.log('some one left');
+  });
+  // socket.broadcast.emit('hi');
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
@@ -13,14 +42,17 @@ async function startApolloServer() {
   });
   await server.start();
   
-  const app = express();
-  server.applyMiddleware({ app });
-  const port = process.env.PORT || 4000;
-  await new Promise((resolve) => app.listen({ port }, resolve));
-  console.log(`Apollo Server 3 listening on port ${port}`);
+  server.applyMiddleware({ app, path: '/graphql' });
+  // const port = process.env.PORT || 4000;
+  // await new Promise((resolve) => app.listen({ port }, resolve));
+  // console.log(`Apollo Server 3 listening on port ${port}`);
 }
 
 startApolloServer();
+ioServer.listen(3000, () => {
+  console.log('listening on 3000');
+});
+
 // server
 //   .listen({port: process.env.PORT || 4000})
 //   .then(({ url }) => console.log('Server is running on localhost:4000'));
